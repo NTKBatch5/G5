ab08eab65fabc56663403978c288290a3f300e0a
EvenStev <110270739+EvenStev@users.noreply.github.com>
2022-08-03 15:38:51 -0400

    Update DemoG5.java
    added

--- src/team/g5/DemoG5.java
+++ src/team/g5/DemoG5.java
@@ -6,6 +6,8 @@ public class DemoG5 {
 		
 		System.out.println("G1 is the BEST.....in their DREAMS");
 		System.out.println("G5 all the WAY");
+		
+		System.out.println(firstNonRepeat("AaBbCDdE"));
 
 	}
 
@@ -15,6 +17,13 @@ public class DemoG5 {
 // Charlene
 		System.out.println("Charlene Parker");
 	}
+	
+	public void firstNonRepeat(String str){
+		str.toLowerCase();
+		for(char i : str.toCharArray()) {
+		if(str.indexOf(i)==str.lastIndexOf(i));
+		System.out.println("i");
+		break;
 
 // Wikly	
 	public static void subtract(int a, int b) {
